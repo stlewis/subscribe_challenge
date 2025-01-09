df8a642a099bc092c6d43793053b8eb44ae7a840
Steve Lewis <steve@decodingsteve.com>
2025-01-08 23:05:14 -0800

    Remember that we created a run.rb file for a reason
    Refactor things that were in `init.rb` to go into `run.rb` so that
    `init.rb` can be properly utilized by specs

--- init.rb
+++ init.rb
@@ -2,20 +2,3 @@ require 'json'
 require './lib/cart_validator'
 require './lib/cart_calculator'
 
-cart = JSON.parse(File.read(ARGV[0]))
-
-validator = CartValidator.new(cart)
-
-if validator.validate!
-  calculator = CartCalculator.new(cart)
-  calculator.calculate!
-
-  puts 'Your Input Cart:'
-  calculator.print_cart
-  puts ''
-
-  puts 'Your Output Cart:'
-  calculator.print_receipt
-else
-  puts validator.errors
-end
--- run.rb
+++ run.rb
@@ -1,1 +1,19 @@
 require './init.rb'
+
+cart = JSON.parse(File.read(ARGV[0]))
+
+validator = CartValidator.new(cart)
+
+if validator.validate!
+  calculator = CartCalculator.new(cart)
+  calculator.calculate!
+
+  puts 'Your Input Cart:'
+  calculator.print_cart
+  puts ''
+
+  puts 'Your Output Cart:'
+  calculator.print_receipt
+else
+  puts validator.errors
+end
